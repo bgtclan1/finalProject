c5056d2912b113dfbacacab12d0ec408eb5a50e9
papu <bgtclan1@gmail.com>
2022-06-06 17:45:11 -0500

    initial commit

---
+++ src/main/java/co/edu/utp/gia/finalProject/FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package co.edu.utp.gia.finalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectApplication.class, args);
+	}
+
+}
